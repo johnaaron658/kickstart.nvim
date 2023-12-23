461e12d897e8c89a50ce9dd6ee303c5d6b2729d6
= <=>
2023-12-23 17:02:56 +0800

    use spaces instead of tabs

--- init.lua
+++ init.lua
@@ -94,6 +94,7 @@ vim.g.neovide_cursor_animate_in_insert_mode = true
 vim.g.neovide_cursor_animate_command_line = true
 vim.g.neovide_cursor_vfx_mode = "pixiedust"
 vim.g.neovide_scale_factor = 0.75
+vim.o.expandtab = true
 
 -- scroll off
 vim.opt.scrolloff = 8
