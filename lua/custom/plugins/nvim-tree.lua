8d325605d19e1b6e897949efc53df4ea1ba7e5b2
John Aaron Q. Alcoseba <johnaaron658@gmail.com>
2024-03-26 17:29:08 +0800

    replace neotree with nvimtree

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-tree/nvim-tree.lua',
+  config = function()
+    require("nvim-tree").setup({
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      }
+    })
+  end
+}
