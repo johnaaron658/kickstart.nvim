d1bca04d127f51b0a5c139868b2a77724c968669
John Aaron Q. Alcoseba <johnaaron658@gmail.com>
2024-03-26 17:51:34 +0800

    fix neotree problems

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,35 +1,35 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 -- vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 --
--- return {
---   "nvim-neo-tree/neo-tree.nvim",
---   version = "*",
---   dependencies = {
---     "nvim-lua/plenary.nvim",
---     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
---     "MunifTanjim/nui.nvim",
---   },
---   config = function ()
---     require('neo-tree').setup {
---       filesystem = {
---         filtered_items = {
---           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
---           hide_dotfiles = false,
---           hide_gitignored = false,
---         },
---         follow_current_file = {
---           enabled = true, -- This will find and focus the file in the active buffer every time
---           --               -- the current file is changed while the tree is open.
---           leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
---         },
---       },
---       buffers = {
---         follow_current_file = {
---           enabled = true, -- This will find and focus the file in the active buffer every time
---           --              -- the current file is changed while the tree is open.
---           leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
---         },
---       },
---     }
---   end,
--- }
+return {
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- version = "*",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+  --   "MunifTanjim/nui.nvim",
+  -- },
+  -- config = function ()
+  --   require('neo-tree').setup {
+  --     filesystem = {
+  --       filtered_items = {
+  --         visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+  --         hide_dotfiles = false,
+  --         hide_gitignored = false,
+  --       },
+  --       follow_current_file = {
+  --         enabled = true, -- This will find and focus the file in the active buffer every time
+  --         --               -- the current file is changed while the tree is open.
+  --         leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+  --       },
+  --     },
+  --     buffers = {
+  --       follow_current_file = {
+  --         enabled = true, -- This will find and focus the file in the active buffer every time
+  --         --              -- the current file is changed while the tree is open.
+  --         leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+  --       },
+  --     },
+  --   }
+  -- end,
+}
