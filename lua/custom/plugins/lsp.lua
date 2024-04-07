6a8c8c16024b445d06df919089398a7688b82855
John Aaron Q. Alcoseba <johnaaron658@gmail.com>
2024-04-08 07:13:19 +0800

    update other lsp setup implementation

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,10 +1,10 @@
 -- Enable the following language servers
 local servers = {
- --  tsserver = {},
- --  html = { filetypes = { 'html', 'twig', 'hbs'} },
- --  cssls = {},
- --  eslint = {},
-	-- csharp_ls = {},
+  --  tsserver = {},
+  --  html = { filetypes = { 'html', 'twig', 'hbs'} },
+  --  cssls = {},
+  --  eslint = {},
+  -- csharp_ls = {},
 
   lua_ls = {
     Lua = {
@@ -17,12 +17,15 @@ local servers = {
 }
 
 -- setup lsps not found in mason
-local other_lsp_setup = function (on_attach, capabilities)
-	local lspconfig = require('lspconfig')
-  lspconfig.dartls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities
-  })
+local other_lsp_setup = function(on_attach, capabilities)
+  local lspconfig = require('lspconfig')
+
+  if lspconfig.dartls ~= nil then
+    lspconfig.dartls.setup({
+      on_attach = on_attach,
+      capabilities = capabilities
+    })
+  end
 end
 
 -- [[ Configure LSP ]]
@@ -38,52 +41,52 @@ local on_attach = function(_, bufnr)
 end
 
 return
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
+{
+  -- LSP Configuration & Plugins
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    -- Automatically install LSPs to stdpath for neovim
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
 
-      -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+    -- Useful status updates for LSP
+    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+    { 'j-hui/fidget.nvim', opts = {} },
 
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-    },
-    config = function()
-      -- mason-lspconfig requires that these setup functions are called in this order
-      -- before setting up the servers.
-      require('mason').setup()
-      require('mason-lspconfig').setup()
+    -- Additional lua configuration, makes nvim stuff amazing!
+    'folke/neodev.nvim',
+  },
+  config = function()
+    -- mason-lspconfig requires that these setup functions are called in this order
+    -- before setting up the servers.
+    require('mason').setup()
+    require('mason-lspconfig').setup()
 
-      -- Setup neovim lua configuration
-      require('neodev').setup()
+    -- Setup neovim lua configuration
+    require('neodev').setup()
 
-      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-      -- Ensure the servers above are installed
-      local mason_lspconfig = require 'mason-lspconfig'
+    -- Ensure the servers above are installed
+    local mason_lspconfig = require 'mason-lspconfig'
 
-      mason_lspconfig.setup {
-        ensure_installed = vim.tbl_keys(servers),
-      }
+    mason_lspconfig.setup {
+      ensure_installed = vim.tbl_keys(servers),
+    }
 
-      mason_lspconfig.setup_handlers {
-        function(server_name)
-          require('lspconfig')[server_name].setup {
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-            filetypes = (servers[server_name] or {}).filetypes,
-          }
-        end,
-      }
+    mason_lspconfig.setup_handlers {
+      function(server_name)
+        require('lspconfig')[server_name].setup {
+          capabilities = capabilities,
+          on_attach = on_attach,
+          settings = servers[server_name],
+          filetypes = (servers[server_name] or {}).filetypes,
+        }
+      end,
+    }
 
-      other_lsp_setup(on_attach, capabilities)
-    end
-  }
+    other_lsp_setup(on_attach, capabilities)
+  end
+}
