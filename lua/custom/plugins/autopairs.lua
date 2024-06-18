47bb26640971412a12ab774023d881eb62cf2e91
John Aaron Q. Alcoseba <johnaaron658@gmail.com>
2024-06-18 20:00:17 +0800

    autopairs, oil, and snippet updates

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -1,7 +1,10 @@
 return {
   "windwp/nvim-autopairs",
   -- Optional dependency
-  dependencies = { 'hrsh7th/nvim-cmp' },
+  dependencies = {
+    'hrsh7th/nvim-cmp',
+    'windwp/nvim-ts-autotag',
+  },
   config = function()
     require("nvim-autopairs").setup {}
     -- If you want to automatically add `(` after selecting a function or method
@@ -11,5 +14,22 @@ return {
       'confirm_done',
       cmp_autopairs.on_confirm_done()
     )
+
+    require('nvim-ts-autotag').setup({
+      opts = {
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false -- Auto close on trailing </
+      },
+      -- Also override individual filetype configs, these take priority.
+      -- Empty by default, useful if one of the "opts" global settings
+      -- doesn't work well in a specific filetype
+      per_filetype = {
+        ["html"] = {
+          enable_close = false
+        }
+      }
+    })
   end,
 }
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,7 +4,15 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function ()
-    require("oil").setup()
+    require("oil").setup({
+      lsp_file_methods = {
+        -- Time to wait for LSP file operations to complete before skipping
+        timeout_ms = 100000,
+        -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+        -- Set to "unmodified" to only save unmodified buffers
+        autosave_changes = true,
+      }
+    })
     vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
   end
 }
